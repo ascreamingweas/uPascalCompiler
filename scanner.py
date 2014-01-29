d724f025a6230653d9b2d82874e57ac540c9975c
ascreamingweas <ascreamingweas@gmail.com>
2014-01-29 13:28:18 -0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,11 +1,28 @@
 import token
+import alpha
 
 """
 This is the scanner portion of our microPascal compiler
 """
 
 #Scan things
+class Scanner():
+ 
+    def openFile(fname):
+        f = open(fname, 'r')
+        return f
 
-if __name__ == "__main__":
-    import sys
-    # Do something if the scanner is executed directly instead of imported
+    def getTokens(fileObject):
+        for line in fileObject:
+            getNextToken(fileObject[line])
+ 
+    def getNextToken(line):
+        foundToken = None
+ 
+        if re.match(regex.alpha, line[0]):
+            print "Found Alpha: " + line[0]
+            foundToken = alpha.AlphaFSA(line)
+ 
+ 
+    def __init__(self):
+        self.token = token.Token()
