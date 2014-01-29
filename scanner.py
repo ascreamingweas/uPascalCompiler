e70fb9686a60fb26c79bce0fa623ba8aeaeb28dd
ascreamingweas <ascreamingweas@gmail.com>
2014-01-29 13:41:09 -0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -7,22 +7,30 @@ This is the scanner portion of our microPascal compiler
 
 #Scan things
 class Scanner():
+    colNum = None
+    lineNum = None
+    token = None
+    lexeme = None
  
     def openFile(fname):
         f = open(fname, 'r')
         return f
 
-    def getTokens(fileObject):
-        for line in fileObject:
-            getNextToken(fileObject[line])
+    def getLexeme():
+        return lexeme
+ 
+    def getLineNumber():
+        return lineNum
+ 
+    def getColumnNumber():
+        return colNum
+
+    def dispatcher(file):
+        for line in file:
+            getNextToken(file[line])
  
     def getNextToken(line):
         foundToken = None
- 
         if re.match(regex.alpha, line[0]):
             print "Found Alpha: " + line[0]
             foundToken = alpha.AlphaFSA(line)
- 
- 
-    def __init__(self):
-        self.token = token.Token()
